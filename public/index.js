505b1b98b30b17f5b0f14b6d7f84e20138d6c14e
rbroekgillz <r.broek@gillz.nl>
2020-06-05 15:09:38 +0200

    Can search with search bar

--- public/index.js
+++ public/index.js
@@ -2,6 +2,27 @@ async function getRandomAdvice() {
     try {
         const res = await fetch('/v1/advice/random');
         const advice = await res.json();
+
+        if (advice.err) {
+            throw new Error();
+        }
+
+        const adviceElement = document.querySelector('.free-advice');
+        adviceElement.textContent = advice.message;
+    } catch (e) {
+        console.log('There was an error.')
+    }
+}
+
+async function searchForAdvice(query) {
+    try {
+        const res = await fetch(`/v1/advice/search/${query}`);
+        const advice = await res.json();
+
+        if (advice.err) {
+            throw new Error();
+        }
+
         const adviceElement = document.querySelector('.free-advice');
         adviceElement.textContent = advice.message;
     } catch (e) {
@@ -10,3 +31,21 @@ async function getRandomAdvice() {
 }
 
 getRandomAdvice();
+
+const button = document.querySelector(".random-advice-button");
+button.addEventListener("click", e => {
+    e.preventDefault();
+    getRandomAdvice();
+});
+
+const searchBar = document.querySelector("#search-bar");
+const searchButton = document.querySelector(".search-button");
+
+console.log({searchBar, searchButton});
+
+searchButton.addEventListener("click", e => {
+    e.preventDefault();
+    searchForAdvice(searchBar.value);
+})
+
+
