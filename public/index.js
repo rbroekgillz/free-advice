3d52cb8e3504b92ec7eea08307dd24207cb337fd
rbroekgillz <r.broek@gillz.nl>
2020-06-05 14:34:29 +0200

    Add random advice on page load

--- public/index.js
+++ public/index.js
@@ -1,1 +1,12 @@
-console.log('Script tag le wurx!')
+async function getRandomAdvice() {
+    try {
+        const res = await fetch('/v1/advice/random');
+        const advice = await res.json();
+        const adviceElement = document.querySelector('.free-advice');
+        adviceElement.textContent = advice.message;
+    } catch (e) {
+        console.log('There was an error.')
+    }
+}
+
+getRandomAdvice();
